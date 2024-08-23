44bb3fcdc86d026dd88a1cdf85463ec2d752633b
anyars-encarta <anyarsencarta@gmail.com>
2024-08-23 15:52:27 +0000

    Create user cards

--- front-end/app/(root)/admin/page.tsx
+++ front-end/app/(root)/admin/page.tsx
@@ -1,8 +1,22 @@
+import UserCard from '@/components/UserCard'
 import React from 'react'
 
 const AdminPage = () => {
   return (
-    <div>AdminPage</div>
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/* LEFT */}
+      <div className='w-full lg:w-2/3'>
+        <div className='flex gap-4 justify-between flex-wrap'>
+          <UserCard type='student' />
+          <UserCard type='teacher' />
+          <UserCard type='parent' />
+          <UserCard type='staff' />
+        </div>
+      </div>
+
+      {/* RIGHT */}
+      <div className='w-full lg:w-1/3'>R</div>
+    </div>
   )
 }
 
