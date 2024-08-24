6f14cbc7a204138cb2b418a5277727575d47d6a0
anyars-encarta <anyarsencarta@gmail.com>
2024-08-24 15:38:08 +0000

    Install Big Calendar

--- front-end/app/(root)/student/page.tsx
+++ front-end/app/(root)/student/page.tsx
@@ -1,5 +1,8 @@
-import Announcement from '@/components/Announcement'
-import EventCalendar from '@/components/EventCalendar'
+'use client';
+
+import Announcement from '@/components/Announcement';
+import BigCalendar from '@/components/BigCalendar';
+import EventCalendar from '@/components/EventCalendar';
 
 const StudentPage = () => {
   return (
@@ -8,6 +11,7 @@ const StudentPage = () => {
       <div className='w-full xl:w-2/3'>
       <div className='h-full bg-white p-4 rounded-md'>
         <h1 className='text-xl font-semibold'>Schedule (4A)</h1>
+        <BigCalendar />
       </div>
       </div>
 
