d096ed71e5b43d84698471c147cb52bffcae06a9
anyars-encarta <anyarsencarta@gmail.com>
2024-08-24 15:07:05 +0000

    Install react-big calendar

--- front-end/app/(root)/student/page.tsx
+++ front-end/app/(root)/student/page.tsx
@@ -1,12 +1,15 @@
 import Announcement from '@/components/Announcement'
 import EventCalendar from '@/components/EventCalendar'
-import React from 'react'
 
 const StudentPage = () => {
   return (
     <div className='p-4 flex gap-4 flex-col xl:flex-row'>
       {/* LEFT */}
-      <div className='w-full xl:w-2/3'></div>
+      <div className='w-full xl:w-2/3'>
+      <div className='h-full bg-white p-4 rounded-md'>
+        <h1 className='text-xl font-semibold'>Schedule (4A)</h1>
+      </div>
+      </div>
 
       {/* RIGHT */}
       <div className='w-full lg:w-1/3 flex flex-col gap-8'>
