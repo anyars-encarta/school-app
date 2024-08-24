2817aef90304eb6120635049608af3c71f426416
anyars-encarta <anyarsencarta@gmail.com>
2024-08-24 16:31:44 +0000

    Add Big Calendar to teacher page

--- front-end/app/(root)/teacher/page.tsx
+++ front-end/app/(root)/teacher/page.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+'use client';
+
+import Announcement from '@/components/Announcement';
+import BigCalendar from '@/components/BigCalendar';
+import EventCalendar from '@/components/EventCalendar';
 
 const TeacherPage = () => {
   return (
-    <div>TeacherPage</div>
+    <div className='flex-1 p-4 flex gap-4 flex-col xl:flex-row'>
+      {/* LEFT */}
+      <div className='w-full xl:w-2/3'>
+      <div className='h-full bg-white p-4 rounded-md'>
+        <h1 className='text-xl font-semibold'>Schedule</h1>
+        <BigCalendar />
+      </div>
+      </div>
+
+      {/* RIGHT */}
+      <div className='w-full lg:w-1/3 flex flex-col gap-8'>
+        {/* <EventCalendar /> */}
+
+        <Announcement />
+      </div>
+    </div>
   )
 }
 
