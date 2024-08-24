c57d377d8abe0403cc1e46ea5b6832e0ba5bfadc
anyars-encarta <anyarsencarta@gmail.com>
2024-08-24 14:58:02 +0000

    Correct right Sidebar

--- front-end/app/(root)/student/page.tsx
+++ front-end/app/(root)/student/page.tsx
@@ -1,8 +1,20 @@
+import Announcement from '@/components/Announcement'
+import EventCalendar from '@/components/EventCalendar'
 import React from 'react'
 
 const StudentPage = () => {
   return (
-    <div>StudentPage</div>
+    <div className='p-4 flex gap-4 flex-col xl:flex-row'>
+      {/* LEFT */}
+      <div className='w-full xl:w-2/3'></div>
+
+      {/* RIGHT */}
+      <div className='w-full lg:w-1/3 flex flex-col gap-8'>
+        <EventCalendar />
+
+        <Announcement />
+      </div>
+    </div>
   )
 }
 
--- front-end/components/Menu.tsx
+++ front-end/components/Menu.tsx
@@ -1,6 +1,7 @@
 import { menuItems } from "@/constants/Menu"
 import Image from "next/image"
 import Link from "next/link"
+import { role } from "@/lib/data";
 
 const Menu = () => {
     return (
@@ -9,18 +10,22 @@ const Menu = () => {
                 <div className='flex flex-col gap-2' key={i}>
                     <span className='hidden lg:block text-gray-400 font-light my-4'>{item.title}</span>
 
-                    {item.items.map((linkItem, index) => (
-                        <Link href={linkItem.href} key={index} className='flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 hover:bg-encSky rounded-lg pl-2'>
-                            <Image
-                                src={linkItem.icon}
-                                alt={linkItem.label}
-                                width={20}
-                                height={20}
-                            />
-
-                            <span className='hidden lg:block'>{linkItem.label}</span>
-                        </Link>
-                    ))}
+                    {item.items.map((linkItem, index) => {
+                        if(linkItem.visible.includes(role)) {
+                           return (
+                                <Link href={linkItem.href} key={index} className='flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 hover:bg-encSkyLight rounded-md pl-2'>
+                                    <Image
+                                        src={linkItem.icon}
+                                        alt={linkItem.label}
+                                        width={20}
+                                        height={20}
+                                    />
+        
+                                    <span className='hidden lg:block'>{linkItem.label}</span>
+                                </Link>
+                            )
+                        }
+                    })}
                 </div>
             ))}
         </div>
