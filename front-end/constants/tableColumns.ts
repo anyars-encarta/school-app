3f697c6a95db0bd0ace80b9cc79d8dc8c3e3e7c7
anyars-encarta <anyarsencarta@gmail.com>
2024-08-25 09:03:32 +0000

    Add teachers list

---
+++ front-end/constants/tableColumns.ts
@@ -0,0 +1,35 @@
+export const columns = [
+    {
+        header: 'Info',
+        accessor: 'info'
+    },
+    {
+        header: 'Teacher ID',
+        accessor: 'teacherId',
+        className: 'hidden md:table-cell'
+    },
+    {
+        header: 'Subjects',
+        accessor: 'subjects',
+        className: 'hidden md:table-cell'
+    },
+    {
+        header: 'Classes',
+        accessor: 'classes',
+        className: 'hidden md:table-cell'
+    },
+    {
+        header: 'Phone',
+        accessor: 'phone',
+        className: 'hidden md:table-cell'
+    },
+    {
+        header: 'Address',
+        accessor: 'address',
+        className: 'hidden md:table-cell'
+    },
+    {
+        header: 'Actions',
+        accessor: 'actions',
+    },
+]
