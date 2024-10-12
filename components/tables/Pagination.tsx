6db10edae3454708efe39a2103548a50c7c2ca78
anyars-encarta <anyarsencarta@gmail.com>
2024-10-12 19:03:47 +0000

    Implement Next and Previous buttons functionalities

--- components/tables/Pagination.tsx
+++ components/tables/Pagination.tsx
@@ -15,7 +15,12 @@ const Pagination = ({ page, count }: { page: number, count: number }) => {
 
     return (
         <div className='p-4 flex items-center justify-between text-gray-500'>
-            <button disabled className='py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed'>Prev</button>
+            <button
+                disabled={page === 1} className='py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed'
+                onClick={() => navigatePage(page - 1)}
+            >
+                Prev
+            </button>
 
             <div className='flex items-center gap-2 text-sm'>
                 {Array.from({ length: Math.ceil(count / ITEM_PER_PAGE) }, (_, i) => i + 1).map((item, index) => (
@@ -29,7 +34,10 @@ const Pagination = ({ page, count }: { page: number, count: number }) => {
                 ))}
             </div>
 
-            <button className='py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed'>Next</button>
+            <button 
+                disabled={page === Math.ceil(count / ITEM_PER_PAGE)} onClick={() => navigatePage(page + 1)} className='py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed'>
+                    Next
+                </button>
         </div>
     )
 }
