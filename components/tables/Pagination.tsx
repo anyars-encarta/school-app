0023b8d868895febd249f8454f49213fea75faf3
anyars-encarta <anyarsencarta@gmail.com>
2024-10-12 18:55:27 +0000

    Add click functionalities to page numbers

--- components/tables/Pagination.tsx
+++ components/tables/Pagination.tsx
@@ -1,7 +1,18 @@
+'use client';
+
 import { ITEM_PER_PAGE } from '@/lib/settings'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const Pagination = ({ page, count }: { page: number, count: number }) => {
+    const router = useRouter();
+
+    const navigatePage = (newPage: number) => {
+        const params = new URLSearchParams(window.location.search)
+        params.set("page", newPage.toString())
+        router.push(`${window.location.pathname}?${params.toString()}`)
+    };
+
     return (
         <div className='p-4 flex items-center justify-between text-gray-500'>
             <button disabled className='py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed'>Prev</button>
@@ -11,6 +22,7 @@ const Pagination = ({ page, count }: { page: number, count: number }) => {
                     <button
                         key={index}
                         className={`px-2 rounded-md ${page === item ? 'bg-encSky' : 'bg-slate-200'}`}
+                        onClick={() => navigatePage(item)}
                     >
                         {item}
                     </button>
