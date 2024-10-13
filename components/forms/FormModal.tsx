383acba6fe7f915735b552f87cd6216ea04faec1
anyars-encarta <anyarsencarta@gmail.com>
2024-10-13 16:04:16 +0000

    Implement student List with search and pagination

--- components/forms/FormModal.tsx
+++ components/forms/FormModal.tsx
@@ -91,7 +91,7 @@ const FormModal = ({
     | 'announcement';
     type: 'create' | 'update' | 'delete';
     data?: any
-    id?: string;
+    id?: number | string;
 }) => {
     const size = type === 'create' ? 'w-8 h-8' : 'w-7 h-7'
     const bgColor = type === 'create' ? 'bg-encYellow' : type === 'update' ? 'bg-encSky' : 'bg-encPurple';
