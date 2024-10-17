b84e967450becde15e1a688eedcd1a375fbe083e
anyars-encarta <anyarsencarta@gmail.com>
2024-10-17 08:16:17 +0000

    Fetch Classes data

--- app/(root)/list/classes/page.tsx
+++ app/(root)/list/classes/page.tsx
@@ -7,45 +7,94 @@ import Link from 'next/link'
 import { classColumns } from '@/constants/tableColumns';
 import FormModal from '@/components/forms/FormModal';
 import { ClassesParams } from '@/app/types';
+import { Announcement, Class, Lesson, Prisma, Student, Teacher } from '@prisma/client';
+import prisma from '@/prisma';
+import { ITEM_PER_PAGE } from '@/lib/settings';
 
-const ClassesList = () => {
-    const renderRow = (item: ClassesParams) => (
-        <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-encSkyLight'>
-            <td className='flex items-center gap-4 p-4'>{item.name}</td>
-            <td className='hidden md:table-cell'>{item.capacity}</td>
-            <td className='hidden md:table-cell'>{item.grade}</td>
-            <td className='hidden md:table-cell'>{item.supervisor}</td>
+type classList = Class & { supervisor: Teacher }
 
-            <td>
-                <div className='flex items-center gap-2'>
-                    {role === 'admin' && (
-                        <>
-                            {/* <Link href={`/list/teachers/${item.id}`}> */}
-                            {/* <button className='flex items-center justify-center rounded-full bg-encSky'>
-                                    <Image src='/update.png' alt='' width={16} height={16} />
-                                </button> */}
-                            <FormModal table='class' type='update' data={
-                                {
-                                    id: 1,
-                                    name: "1A",
-                                    capacity: 20,
-                                    grade: 1,
-                                    supervisor: "Joseph Padilla",
-                                }
-                            } />
-                            {/* </Link> */}
+const renderRow = (item: classList) => (
+    <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-encSkyLight'>
+        <td className='flex items-center gap-4 p-4'>{item.name}</td>
+        <td className='hidden md:table-cell'>{item.capacity}</td>
+        <td className='hidden md:table-cell'>{item.name[0]}</td>
+        <td className='hidden md:table-cell'>{item.supervisor.name + ' ' + item.supervisor.surname}</td>
 
-
-                            {/* <button className='flex items-center justify-center rounded-full bg-encPurple'>
-                                <Image src='/delete.png' alt='' width={16} height={16} />
+        <td>
+            <div className='flex items-center gap-2'>
+                {role === 'admin' && (
+                    <>
+                        {/* <Link href={`/list/teachers/${item.id}`}> */}
+                        {/* <button className='flex items-center justify-center rounded-full bg-encSky'>
+                                <Image src='/update.png' alt='' width={16} height={16} />
                             </button> */}
-                            <FormModal table='class' type='delete' id={item.id} />
-                        </>
-                    )}
-                </div>
-            </td>
-        </tr>
-    )
+                        <FormModal table='class' type='update' data={
+                            {
+                                id: 1,
+                                name: "1A",
+                                capacity: 20,
+                                grade: 1,
+                                supervisor: "Joseph Padilla",
+                            }
+                        } />
+                        {/* </Link> */}
+
+
+                        {/* <button className='flex items-center justify-center rounded-full bg-encPurple'>
+                            <Image src='/delete.png' alt='' width={16} height={16} />
+                        </button> */}
+                        <FormModal table='class' type='delete' id={item.id} />
+                    </>
+                )}
+            </div>
+        </td>
+    </tr>
+);
+
+const ClassesList = async ({
+    searchParams,
+}: {
+    searchParams: { [key: string]: string | undefined }
+}) => {
+    const { page, ...queryParams } = searchParams;
+
+    const p = page ? parseInt(page) : 1;
+
+    // URL PARAMS CONDITIONS
+    const query: Prisma.ClassWhereInput = {};
+
+    if (queryParams) {
+        for (const [key, value] of Object.entries(queryParams)) {
+            if (value !== undefined) {
+                switch (key) {
+                    case "supervisorId":
+                        query.supervisorId = value;
+                        break;
+                    case "search":
+                        query.name = {
+                            contains: value,
+                            mode: "insensitive",
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    const [classes, count] = await prisma.$transaction([
+        prisma.class.findMany({
+            where: query,
+            include: {
+                supervisor: true,
+            },
+            take: ITEM_PER_PAGE,
+            skip: ITEM_PER_PAGE * (p - 1)
+        }),
+
+        prisma.class.count({ where: query })
+    ]);
 
     return (
         <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
@@ -76,10 +125,10 @@ const ClassesList = () => {
             </div>
 
             {/* LIST */}
-            <ClassTable classColumns={classColumns} renderRow={renderRow} data={classesData} />
+            <ClassTable classColumns={classColumns} renderRow={renderRow} data={classes} />
 
             {/* PAGINATION */}
-            <Pagination />
+            <Pagination page={p} count={count} />
         </div>
     )
 }
