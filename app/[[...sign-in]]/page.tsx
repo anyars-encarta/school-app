c23cf45caff2508bc70a661c1d34f3656bb10889
anyars-encarta <anyarsencarta@gmail.com>
2024-10-23 18:25:56 +0000

    Install Clerm elements and Create custom clerk component

--- app/[[...sign-in]]/page.tsx
+++ app/[[...sign-in]]/page.tsx
@@ -1,5 +1,57 @@
-import { SignIn } from '@clerk/nextjs'
+'use client'
 
-export default function Page() {
-  return <SignIn forceRedirectUrl={}/>
+import * as Clerk from '@clerk/elements/common'
+import * as SignIn from '@clerk/elements/sign-in'
+import { User } from 'lucide-react'
+import Image from 'next/image'
+
+import React from 'react'
+
+const LoginPage = () => {
+    return (
+        <div className='h-screen flex items-center justify-center bg-encSkyLight'>
+            
+            <SignIn.Root>
+                <SignIn.Step name="start" className="bg-white p-12 rounded-md shadow-2xl flex flex-col gap-2">
+                    <h1 className='text-xl font-bold flex items-center gap-2 justify-center'>
+                        <Image src='/logo.png' alt='' width={24} height={24}/>
+                        E-Montessori
+                    </h1>
+
+                    <h2 className='text-gray-400 text-center'>Sign in to your account</h2>
+
+                    <Clerk.GlobalError className="text-xs text-red-400" />
+
+                    <Clerk.Field name="identifier" className="flex flex-col gap-2">
+                        <Clerk.Label className="text-xs text-gray-500">Username</Clerk.Label>
+                        <Clerk.Input
+                            type="text"
+                            required
+                            className="p-2 rounded-md ring-1 ring-gray-300"
+                        />
+                        <Clerk.FieldError className="text-xs text-red-400" />
+                    </Clerk.Field>
+
+                    <Clerk.Field name="password" className="flex flex-col gap-2">
+                        <Clerk.Label className="text-xs text-gray-500">Password</Clerk.Label>
+                        <Clerk.Input
+                            type="password"
+                            required
+                            className="p-2 rounded-md ring-1 ring-gray-300"
+                        />
+                        <Clerk.FieldError className="text-xs text-red-400" />
+                    </Clerk.Field>
+
+                    <SignIn.Action
+                        submit
+                        className="bg-blue-500 text-white my-1 rounded-md text-sm p-[10px]"
+                    >
+                        Sign In
+                    </SignIn.Action>
+                </SignIn.Step>
+            </SignIn.Root>
+        </div>
+    )
 }
+
+export default LoginPage
