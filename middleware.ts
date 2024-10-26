53f7e009bff46cdddaef0b9ba125eaa3c89a5058
anyars-encarta <anyarsencarta@gmail.com>
2024-10-26 10:58:31 +0000

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,37 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { routeAccessMap } from "./lib/settings";
+import { NextResponse } from "next/server";
 
 // const isProtectedRoute = createRouteMatcher(['/admin', '/teacher', '/student', '/parent'])
-const matchers = Object.keys(routeAccessMap).map(route => ({
+const matchers = Object.keys(routeAccessMap).map((route) => ({
   matcher: createRouteMatcher([route]),
-  allowRoles: routeAccessMap[route]
+  allowedRoles: routeAccessMap[route],
 }));
 
 console.log('The matchers', matchers);
 
-export default clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware((auth, req) => {
   // if (isProtectedRoute(req)) await auth.protect()
 
   const { sessionClaims } = auth();
-})
+
+  // if (!sessionClaims) {
+  //   console.log('No session claims found. User may not be authenticated.');
+  //   return NextResponse.redirect(new URL('/', req.url)); // Redirect to login or similar
+  // }
+
+  console.log('The claims', sessionClaims)
+
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+
+  console.log('The role', role)
+
+  for (const { matcher, allowedRoles } of matchers) {
+    if (matcher(req) && !allowedRoles.includes(role!)) {
+      return NextResponse.redirect(new URL(`/${role}`, req.url))
+    }
+  }
+});
 
 export const config = {
   matcher: [
